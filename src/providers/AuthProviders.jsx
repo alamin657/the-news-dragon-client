b04425cd018c23b013d447d770dd03bd47467876
Alamin <alamin017514@gmail.com>
2023-04-25 22:11:41 +0600

    added the-news-dragon-clinet

---
+++ src/providers/AuthProviders.jsx
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
+export const AuthContext = createContext(null)
+const auth = getAuth(app);
+const user = null;
+const AuthProviders = ({ children }) => {
+    const authInfo = { user }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
